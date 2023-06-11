7c0db42bca76368776cfe21ee100a1078d694b20
saif96-max <kattan.saifali@gmail.com>
2023-06-11 22:42:45 +0200

    modification

--- basic-code.py
+++ basic-code.py
@@ -1,5 +1,11 @@
 # This is my first code in python ;)
 
 print("Hello World")
+print(123)
+print(True)
+print(False)
 
-# print ---- c'est pour faire sorti le contenu dans le terminal \
+# print ---- c'est pour faire sorti le contenu dans le terminal 
+# qeulque ce soit le contenu string ou number
+
+
