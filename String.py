86f36e551a1ad17d69ac13de1fe3d364298a30db
saif96-max <kattan.saifali@gmail.com>
2023-06-12 20:46:50 +0200

    The different type to use the string

---
+++ String.py
@@ -0,0 +1,20 @@
+# en string 
+# on écrit le contenu dans un virgule ('') ou ("") pour le string
+
+# PAR EXEMPLE
+
+name = ('HELLO WORLD')
+print(name)
+name1 = ("HELLO WORLD")
+print(name1)
+
+# pour écrire un contenu assez long en string, on utilse le triple virgule (""")
+
+test = (""" HELLO EVERY ONE , THIS IS A LONGUE TEST IN PYTHON WHEN WE USE THE EXEMPLE""")
+print(test)
+
+# pour écrire un contenu et aller a linge , on utilise le anti-slache (\n)
+
+test1 = ("HELLO EVERY ONE \n THIS IS A LONGUE TEST IN PYTHON")
+print(test1)
+
