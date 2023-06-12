78d0e07d5ee80755df6d646dfb9e36d089dcb822
saif96-max <kattan.saifali@gmail.com>
2023-06-12 21:20:57 +0200

    continuation of the previous topic

--- String.py
+++ String.py
@@ -19,3 +19,17 @@ test1 = ("HELLO EVERY ONE \n THIS IS A LONGUE TEST IN PYTHON")
 print(test1)
 
 
+#pour cibler un caractère ou l'imprimer , on utilise un list [] pour cibler 
+#un ou plusieurs
+
+test2 = ("python is a programming language")
+print(test2[0])
+print(test2[0:6]) # un tableau commence toujours par Zéro et bien pas en commençant par 1
+# par exemple comme le mot ---python-- c'est six caractéres, donc de 0 a 5 mais attention pour afficher 
+# le dernier caractère on souhite afficher dans le mot (python), nous deveons ajouter 1 de plus, 
+# du coup cela s'ecrit comme ca [0:6] ===> python
+
+print(test2[-1])
+# pour afficher le dernier carctere d'un tableau or list en englais, c'est par ecrire [-1]
+# comme dans l'exemple ci-dessus
+#  
