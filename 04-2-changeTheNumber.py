a6c83f2d913e809a5318614d5f23f4d57956eb3c
Saif Ali <kattan.saifali@gmail.com>
2023-06-15 22:46:43 +0200

    change the type of number in python

---
+++ 04-2-changeTheNumber.py
@@ -0,0 +1,31 @@
+#là , nous allons voir comment peut on changer les number vers string 
+#et l'inverse 
+
+#function float() -----> return number float 
+#function str() ----> return "string"
+#function int()  ----> return number integer 
+#function type() ----> savoir le type de l'objet 
+
+
+changeTheNumber = (7+7)
+# print(changeTheNumber + "years old" ) #------>TypeError: unsupported operand type(s) for +: 'int' and 'str'
+#nous pouvons pas faire cete operation entre un strign et Number. pour resoudre ce problem 
+# on va mettre le mot (str)deveant le variable pour excuter cette operation 
+
+
+print(str(changeTheNumber) + " " + "years old") #---> 14 years old
+#le plus (+) dans le print s'appelle concaténation ou (concatenation --> en anglais)  
+
+# on peut changer les nombres type integer (عدد صحيح) a nombres type folat ( رقم عشري)
+
+print(type(15)) # ---><class 'int'>
+
+print(float(15)) #----> nous aurons le float number (15.0)
+
+# on peut tres bien faire l'inverse que l'exemple ci-dessus
+# Par exemple]
+
+print(int(15.6)) # -----> nous aurons le integer number (15)
+
+
+
