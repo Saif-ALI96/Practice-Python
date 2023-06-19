f78f8f84fb2f14c9be057ba7ee640340e81cd571
Saif Ali <kattan.saifali@gmail.com>
2023-06-19 21:54:30 +0200

    exercis to pratice if

---
+++ Conditional-branching-if/11-2-exercis-if.py
@@ -0,0 +1,31 @@
+
+
+
+age = int(input("enter your age: "))
+if age >= 6 and age <=9:
+    print("Age between six to nine years old.")
+
+elif age >=10 and age <=15:
+    print("your are not a child!, you are a teenager")
+else:
+    print("you're an adult!")
+
+
+
+#exercis calculate a student's result
+
+stutent = int(input('entre your result : '))
+
+if stutent >= 90 :
+    print ("A+")
+
+elif  stutent >= 80:
+    print ('B+')
+
+elif stutent >= 70:
+    print ('C')
+
+elif stutent >= 50 :
+    print ('D')
+else:
+    print ('F')
