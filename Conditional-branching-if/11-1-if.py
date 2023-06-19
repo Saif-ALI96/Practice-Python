ea0cf64221dfbcad7454f8fe49a86a76beb4c2d0
Saif Ali <kattan.saifali@gmail.com>
2023-06-19 21:53:18 +0200

    change the name

---
+++ Conditional-branching-if/11-1-if.py
@@ -0,0 +1,32 @@
+# ajourd'hui nous allons voir conditional if in python
+#En Python, l'instruction 'if' est utilisée pour exécuter du code de manière conditionnelle.
+# Elle vous permet de spécifier une condition, et si cette condition est vraie, 
+#le bloc de code associé à l'instruction 'if' sera exécuté. 
+#Si la condition est fausse, le bloc de code sera ignoré.
+#La syntaxe d’une instruction ‘if’ se présente comme suit :
+
+# if , elif , else
+
+x = 5 #on prend un entier en entrée
+y= 10
+if x < y:
+    print('y is greater than x') 
+    #------> y is greater than x
+
+test = True
+if test == True:
+    print("test is correct")
+    #---> test is correct
+
+if test: # une maniere in python te dire c'est vrai 
+    print ("test is true ")
+    #---> test is true
+
+test2 = False  
+if not test2: # une maniere te dire que la variable est False
+    print ('test is false ')
+    #---> test is false
+
+
+
+
