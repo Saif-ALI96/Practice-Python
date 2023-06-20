9ef1b253e893ad5869b04672fa20e17d875ba533
Saif Ali <kattan.saifali@gmail.com>
2023-06-20 22:30:10 +0200

    the loop for

---
+++ Repetition-loop-in-python/13-1-for.py
@@ -0,0 +1,12 @@
+# the loop (boucle) in python ce sont les ---for-- et ----while---
+
+
+#En programmation, les boucles sont utilisées pour exécuter de manière répétée un bloc de code jusqu'à ce qu'une certaine condition soit satisfaite. 
+#Elles vous permettent d'automatiser des tâches répétitives et de traiter des collections de données.
+# Python propose deux principaux types de boucles : la boucle "for" (pour) et la boucle "while" (tant que).
+
+# La première ---for -- 
+#est plus efficace lorsque le nombre d’éléments à par courir est connu avant l’exécution du script ; 
+# celle-ci est mieux adaptée aux listes ou tuples. Cependant elle ne peut pas être interrompue en particulier si on veut revenir sur
+#un élément précédent dans une liste.
+
