95ec552190ec08bd676b87a22bb073a606b4ba2b
Saif Ali <kattan.saifali@gmail.com>
2023-06-20 22:30:54 +0200

    the loop while

---
+++ Repetition-loop-in-python/13-2-while.py
@@ -0,0 +1,7 @@
+
+#La seconde ----while---  permet de contrôler la fin de la boucle avec une variable qui doit toujours avoir comme valeur True.
+# Elle n’a pas besoin d’avoir connaissance du nombre d’itérations possibles car 
+#il faudrait seulement compter au fur et à mesure de chaque itération la valeur de cette variable.
+
+#Exemple:
+
