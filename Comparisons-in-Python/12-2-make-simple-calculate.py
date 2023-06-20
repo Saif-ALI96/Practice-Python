8f3c746ed552d0d45d7ecddb3f7779bfd8bb8992
Saif Ali <kattan.saifali@gmail.com>
2023-06-20 21:44:27 +0200

    exercis about make a simple calculator

---
+++ Comparisons-in-Python/12-2-make-simple-calculate.py
@@ -0,0 +1,20 @@
+
+# exercis
+
+number1 = float(input('entre your number : '))
+opreteur = input()
+number2 = float(input('entre another number:'))
+if opreteur == '+':
+    print (number1 + number2)
+elif opreteur == '-':
+    print (number1 - number2)
+
+elif opreteur == '*':
+    print (number1 * number2)
+elif opreteur == '/':
+    print(number1 / number2)
+
+elif opreteur == '^'
+    print (number1 ** number2)
+else:
+    print ('error')
