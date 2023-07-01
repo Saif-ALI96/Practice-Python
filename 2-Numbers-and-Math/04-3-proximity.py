dd7a1f00f715bbc9fb78b8a0cc0fde8ca0ecfe4c
Saif Ali <kattan.saifali@gmail.com>
2023-07-01 23:02:40 +0200

    update

---
+++ 2-Numbers-and-Math/04-3-proximity.py
@@ -0,0 +1,14 @@
+
+
+
+
+#Pour arrondir les nombres, nous devons utiliser
+#la fonction round()  (عمليه تقريب الاعداد العشريه)
+#La syntaxe est la suivante:
+
+print(round(13.5)) #----> nous aurons le chiffre 14 
+print(round(9.4)) # ----> nous aurons le chiffre 9
+
+# de 0 jusque a 4 le number reste tel qu'il est 
+# de 5 jusque a 9 le number agumente a la prochain chiffre 
+
