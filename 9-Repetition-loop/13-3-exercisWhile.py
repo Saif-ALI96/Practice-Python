a9696a9a5b4f993b9fb7a317bfa8d68aeeaede2c
Saif Ali <kattan.saifali@gmail.com>
2023-07-10 12:28:12 +0200

    update the file

---
+++ 9-Repetition-loop/13-3-exercisWhile.py
@@ -0,0 +1,19 @@
+
+#question  = input('Which city is called the city of love?')
+answer = 'Paris'
+answerOfUser = ''
+count = 0
+lose = False
+
+while answer!= answerOfUser and not lose:
+
+    if count < 3 :
+       answerOfUser  = input('Which city is called the city of love?')
+       count += 1
+    else:
+     lose = True
+if lose:
+    print('you lose')
+
+else:
+    print('you win')
