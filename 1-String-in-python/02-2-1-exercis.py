fda369b1f976496a6a969ebb4e4244cbbdf099ca
Saif Ali <kattan.saifali@gmail.com>
2023-07-21 08:37:32 +0200

    update the file

---
+++ 1-String-in-python/02-2-1-exercis.py
@@ -0,0 +1,15 @@
+#  execis 
+#  faire les vleurs de la list en majuscule  et miniscules
+# par exemple :
+
+my_list = ["hello", "world"]
+for item in my_list:
+    new_item=str(item).upper()
+    print("New Item:",new_item)
+
+#  ou 
+
+liste = ['css', 'html', 'python']
+for loop in range(len(liste)):
+    new_upper_list= liste[loop].upper()
+    print(new_upper_list)
