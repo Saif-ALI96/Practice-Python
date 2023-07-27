8f69bc19ee8943d60e23292fb5d918db404e4ed2
Saif Ali <kattan.saifali@gmail.com>
2023-07-27 11:30:06 +0200

    update the file

--- 2-Numbers-and-Math/04-3-proximity.py
+++ 2-Numbers-and-Math/04-3-proximity.py
@@ -11,4 +11,14 @@ print(round(9.4)) # ----> nous aurons le chiffre 9
 
 # de 0 jusque a 4 le number reste tel qu'il est 
 # de 5 jusque a 9 le number agumente a la prochain chiffre 
+# et on continue avec cette methode pour chaque nombre entier qui suit...
+#pour afficher tous les resultats des numeros entiers entre -7 et
+#-1 incluant il faut ecrire une boucle for
+#  par exemple :
+
+for i in range(2,6):    
+
+    print(i,round(float((abs(i)+1.0))))
+    
+
 
