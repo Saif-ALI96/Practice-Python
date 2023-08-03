992780f389c61ba79c7b8b6683ecf82f55d37248
Saif Ali <kattan.saifali@gmail.com>
2023-08-03 23:30:24 +0200

    update the file

--- 4-Tuples-and-dictionary/07-tuples.py
+++ 4-Tuples-and-dictionary/07-tuples.py
@@ -4,6 +4,8 @@
 
 tuple1 = (20,"<NAME>",3546789)
 tuple1[0] = 5 # ----> TypeError: 'tuple' object does not support item assignment
-print(tuple1) #on affiche le tuple ici, il est un objet donc pas de point d'interrogation à la fin.
+
+print(tuple1)
+ #on affiche le tuple ici, il est un objet donc pas de point d'interrogation à la fin.
 
 # donc on utilise les tuples pour les element qu'on veut jamais les modifier comme on fait avec la list (tableau)
