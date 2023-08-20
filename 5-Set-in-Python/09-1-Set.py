06585b3e6dfc4a51ec059d4ffcd13a3b117d27f8
Saif Ali <kattan.saifali@gmail.com>
2023-08-20 22:00:21 +0200

    update the file

--- 5-Set-in-Python/09-1-Set.py
+++ 5-Set-in-Python/09-1-Set.py
@@ -1,4 +1,5 @@
 #LES SETS EN PYTHON
+'''
 #Comme les dictionnaires, les sets sont des collections d'éléments non-ordonnées.
 
 #Dans un set, chaque élément doit être unique et immuable , c’est-à- dire qu'il ne peut pas être modifié.
@@ -7,7 +8,7 @@
 
 #En revanche, le set en lui-même peut être modifié ! On peut y ajouter des objets et aussi en supprimer.
 #Les sets sont souvent utilisés pour contrôler les doublons et effectuer des opérations sur les ensembles comme 
-#les unions, les différences, les intersections, etc.
+#les unions, les différences, les intersections, etc.'''
 #Exemple :
 
 a = {10,'python',25} #Crée une liste avec trois éléments uniques (10, 'python' and 25)
