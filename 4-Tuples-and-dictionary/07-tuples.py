6e1e20c27917512c8835d67b368052d6eff19dc3
Saif Ali <kattan.saifali@gmail.com>
2023-08-20 21:59:44 +0200

    update the file

--- 4-Tuples-and-dictionary/07-tuples.py
+++ 4-Tuples-and-dictionary/07-tuples.py
@@ -2,10 +2,10 @@
 # les tuples sont un peu près comme les list sauf , on peut pas les modifier une fois qu'elle est créée !
 #par exemple :
 
-tuple = (20,"<NAME>",3546789)
-tuple[0] = 5 # ----> TypeError: 'tuple' object does not support item assignment
+tuple1 = (20,"<NAME>",3546789)
+tuple1[0] = 5 # ----> TypeError: 'tuple' object does not support item assignment
 
-print(tuple)
+print(tuple1)
  #on affiche le tuple ici, il est un objet donc pas de point d'interrogation à la fin.
 
 # donc on utilise les tuples pour les element qu'on veut jamais les modifier comme on fait avec la list (tableau)
