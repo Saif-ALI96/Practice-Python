268caf7abc1ab11545a527934faf522d7bbbee1b
Saif Ali <kattan.saifali@gmail.com>
2023-08-21 22:55:15 +0200

    update the file

--- 9-Repetition-loop/13-1-for.py
+++ 9-Repetition-loop/13-1-for.py
@@ -28,8 +28,8 @@ for test1 in 'HELLO WORLD':
    # break #on sort de la boucle si on veut, cela permet de ne
 #pas avoir un code qui s'execute toujours
 
-test = ['hello','world','bonjour']
-for exemple in test:
+test2 = ['hello','world','bonjour']
+for exemple in test2:
     print(exemple)
      #---> hello
          # world
