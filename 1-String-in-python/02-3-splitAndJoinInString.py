f90dcbf0deb2ca88f9507365cbc8dd4d3331299c
Saif Ali <kattan.saifali@gmail.com>
2023-08-22 21:56:46 +0200

    update the file

--- 1-String-in-python/02-3-splitAndJoinInString.py
+++ 1-String-in-python/02-3-splitAndJoinInString.py
@@ -25,8 +25,8 @@ print(nomDeLangages3.split("-",3)) #----> ['python', 'Php', 'Css', 'Html-Java-Ja
 #le function// .rsplit() -----> c'est le même fonctionnement de .split(), 
 # mais il va partir de la droite vers la gauche.
 
-nomDeLangages5 = "python-Php-Css-Html-Java-JavaScript"
-print(nomDeLangages5.rsplit("-", 3)) 
+nomDeLangages = "python-Php-Css-Html-Java-JavaScript"
+print(nomDeLangages.rsplit("-", 3)) 
 
 #-----> ['python-Php-Css', 'Html', 'Java', 'JavaScript']
 
